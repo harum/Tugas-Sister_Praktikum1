849cb82c1774810bbd3aee67c0e7f7f64e8addf6
Herleeyandi Markoni <herleeyandi@gmail.com>
2014-03-09 23:58:37 +0700

    Baca file sudah lengkap termasuk dokumentasi dan semua syntax output, hanya saja masih tipe void, tapi nanti bisa diubah jadi return string.

--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -18,7 +18,7 @@ public class Server {
     public static void main(String[] args) {
         // TODO code application logic here
         ControlFile coba = new ControlFile();
-        coba.bacaHari("Jumat");
+        coba.bacaCuaca("Hujan");
     }
     
 }
